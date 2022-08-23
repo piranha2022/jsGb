3032a0e30290f4d35cbeff45d793e23c560f59c6
Александр <100513728+piranha2022@users.noreply.github.com>
2022-08-23 19:16:51 +0400

    Add files via upload

---
+++ jsscript.js
@@ -0,0 +1,25 @@
+// Задание № 1
+
+let tC = 30;
+let tF;
+tF = (9 / 5) * tC + 32;
+alert("температура по Фаренгейту = " + tF);
+// в консоль
+console.log("температура по Фаренгейту = " + tF);
+
+// Задание № 2
+
+let admin;
+let name;
+name = "Василий";
+admin = name;
+alert(admin);
+// в консоль
+console.log(admin);
+
+// Задание № 3
+let a = 1000 + "108";
+alert('1000 + "108" = ' + a)
+// в консоль
+console.log(a);
+
