2c99cb7cc6daa91edf5eedbb9c921fbc06180814
Александр <a.hmelevskii@mail.ru>
2022-09-09 11:24:26 +0400

    ДЗ № 5 Пока не продвинулся

--- task5.js
+++ task5.js
@@ -10,7 +10,8 @@ function drawChess() {
     let flag = true;
 
 
-
+    // let alfabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+    // let num = ['1', '2', '3', '4', '5', '6', '7', '8'];
 
     let figure = {
         0: ['-263px -19px', '-484px -22px', '-372px -17px', '-150px -16px', '-150px -16px', '-372px -17px', '-484px -22px', '-263px -19px'],
@@ -42,15 +43,81 @@ function drawChess() {
 drawChess();
 
 
+
 console.log('======= Задание № 2 ========');
 
 // 2. Сделать генерацию корзины динамической: верстка корзины не должна находиться в HTML-структуре. Там должен быть только div, в который будет вставляться корзина, сгенерированная на базе JS:
 // 2.1. Пустая корзина должна выводить строку «Корзина пуста»;
 // 2.2. Наполненная должна выводить «В корзине: n товаров на сумму m рублей».
 
-console.log('======= Задание № 3 ========');
+
+let basket = {
+    product: [{
+        name: "галстук",
+        price: 50
+    },
+    {
+        name: "рубашка",
+        price: 100
+    },
+    {
+        name: "ботинки",
+        price: 150
+    },
+    {
+        name: "брюки",
+        price: 200
+    }],
+
+    allproduct() {
+        let result = null;
+        for (let elem of this.product) {
+            result += elem.price;
+
+        }
+        return result;
+    }
+}
 
 
-// 3*. Сделать так, чтобы товары в каталоге выводились при помощи JS:
-// 3.1. Создать массив товаров (сущность Product);
-// 3.2. При загрузке страницы на базе данного массива генерировать вывод из него. HTML-код должен содержать только div id=”catalog” без вложенного кода. Весь вид каталога генерируется JS.
+function renderCart() { // функция генерация корзины
+    const cartBody = document.querySelector(".shop"); // получение блока корзины
+    const productInnerCart = [];    // создание масива иметирующего наличие или отсутствие товаров
+    for (let i = 0; i < 2; i++) {   // получение и добавление товаров из объекта cart
+        productInnerCart.push(basket.product[i])
+    }
+    if (productInnerCart == 0) { // проверка на наличие товаров в корзине
+        cartBody.insertAdjacentHTML("beforeend", `Корзина пуста`)
+    } else {
+        let m = null // создание переменной для хранения суммы товаров 
+        productInnerCart.forEach(element => {
+            m += element.price * element.count // генерация суммы товаров на основе количества и цен товаров 
+        });
+        cartBody.insertAdjacentHTML("beforeend", `
+        В корзине: ${productInnerCart.length} товаров на сумму ${m} рублей
+        `) // вывод количества и суммы товаров .
+    }
+
+
+
+}
+renderCart()
+
+function renderCatalog() { // функция генерации каталога на основе объекта cart
+    const catalogBody = document.querySelector("#catalog"); // получение блока каталога
+
+
+    for (const product of basket.product) { // получение массива из объекта 
+        catalogBody.insertAdjacentHTML("afterbegin", `  
+        <div class="catalog__product">
+            <h2 class="product-heading">${product.name}</h2>
+            <img class="product-img" src="${product.img}" alt="${product.name}">
+            <span class=product-price>${product.price}</span>
+            <button>Добавить в корзину</button>
+        </div>
+    `) // добавление разметки собраной на основе массива из объекта 
+
+
+    }
+}
+renderCatalog()
