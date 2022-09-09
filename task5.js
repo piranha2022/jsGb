cd55b3d3fceec2f8a4691bb0c49d1f24a2f93b2f
Александр <a.hmelevskii@mail.ru>
2022-09-09 13:51:33 +0400

    ДЗ № 5

--- task5.js
+++ task5.js
@@ -1,50 +1,50 @@
 
-console.log('======= Задание № 1 ========');
+
 
 
 // 1. Создать функцию, генерирующую шахматную доску. При этом можно использовать любые html-теги по своему желанию. Доска должна быть разлинована соответствующим образом, т.е. чередовать черные и белые ячейки. Строки должны нумероваться числами от 1 до 8, столбцы – латинскими буквами A, B, C, D, E, F, G, H. (использовать createElement / appendChild)
 
-function drawChess() {
-    let mainBlock = document.querySelector('.main-block');
-    let block;
-    let flag = true;
+function renderchessboard() {
+    const chessBoard = document.querySelector(".chess-board");
+    const refCell = document.createElement("div");
+    refCell.classList.add("chess-cell");
+    const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H"];
 
 
-    // let alfabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
-    // let num = ['1', '2', '3', '4', '5', '6', '7', '8'];
-
-    let figure = {
-        0: ['-263px -19px', '-484px -22px', '-372px -17px', '-150px -16px', '-150px -16px', '-372px -17px', '-484px -22px', '-263px -19px'],
-        1: ['-595px -19px', '-595px -19px', '-595px -19px', '-595px -19px', '-595px -19px', '-595px -19px', '-595px -19px', '-595px -19px'],
-        6: ['-595px -116px', '-595px -116px', '-595px -116px', '-595px -116px', '-595px -116px', '-595px -116px', '-595px -116px', '-595px -116px'],
-        7: ['-263px -116px', '-484px -116px', '-372px -116px', '-150px -116px', '-150px -116px', '-372px -116px', '-484px -116px', '-263px -116px'],
-    };
-
-
-    for (let i = 0; i < 8; i++) {
-        for (j = 0; j < 8; j++) {
-            if (j == 0) flag = !flag;
-
-            block = document.createElement('div');
-            if (flag) block.className = 'block black';
-            else block.className = 'block white';
-
-            if (figure[i] !== undefined && figure[i][j] !== undefined) {
-                block.style.backgroundImage = 'url(сhess_symbols_set_.png)';
-                block.style.backgroundPosition = figure[i][j];
+    for (let colomn = 0; colomn <= 9; colomn++) {
+        if (colomn == 0 || colomn == 9) {
+            for (let rows = 0; rows <= 9; rows++) {
+                let cell = refCell.cloneNode(true);
+                chessBoard.appendChild(cell);
+                if (rows > 0 && rows < 9) {
+                    cell.textContent = rows
+                }
             }
+        }
+        else {
+            for (let rows = 0; rows <= 9; rows++) {
+                let cell = refCell.cloneNode(true);
+                chessBoard.appendChild(cell);
 
-            mainBlock.appendChild(block);
-            flag = !flag;
+                if (rows == 0 || rows == 9) {
+
+                    cell.textContent = alphabet[colomn - 1]
+                } else {
+                    if (rows % 2 != 0 && colomn % 2 != 0 || colomn % 2 == 0 && rows % 2 == 0) {
+                        cell.classList.add("chess-cell_black");
+                    } else {
+                        cell.classList.add("chess-cell_white");
+                    }
+                }
+            }
         }
     }
 }
-
-drawChess();
-
+renderchessboard();
 
 
-console.log('======= Задание № 2 ========');
+
+
 
 // 2. Сделать генерацию корзины динамической: верстка корзины не должна находиться в HTML-структуре. Там должен быть только div, в который будет вставляться корзина, сгенерированная на базе JS:
 // 2.1. Пустая корзина должна выводить строку «Корзина пуста»;
@@ -54,48 +54,46 @@ console.log('======= Задание № 2 ========');
 let basket = {
     product: [{
         name: "галстук",
-        price: 50
+        price: 50,
+        img: 'img/galstuk.svg'
     },
     {
         name: "рубашка",
-        price: 100
+        price: 100,
+        img: 'img/rubashka.svg'
     },
     {
         name: "ботинки",
-        price: 150
+        price: 150,
+        img: "img/botinki-1.jpg"
     },
     {
         name: "брюки",
-        price: 200
-    }],
+        price: 200,
+        img: "img/bruki.svg"
+    },
 
-    allproduct() {
-        let result = null;
-        for (let elem of this.product) {
-            result += elem.price;
+    ],
 
-        }
-        return result;
-    }
 }
 
 
-function renderCart() { // функция генерация корзины
-    const cartBody = document.querySelector(".shop"); // получение блока корзины
-    const productInnerCart = [];    // создание масива иметирующего наличие или отсутствие товаров
-    for (let i = 0; i < 2; i++) {   // получение и добавление товаров из объекта cart
+function renderCart() {
+    const cartBody = document.querySelector(".shop");
+    const productInnerCart = [];
+    for (let i = 0; i < 2; i++) {
         productInnerCart.push(basket.product[i])
     }
-    if (productInnerCart == 0) { // проверка на наличие товаров в корзине
+    if (productInnerCart == 0) {
         cartBody.insertAdjacentHTML("beforeend", `Корзина пуста`)
     } else {
-        let m = null // создание переменной для хранения суммы товаров 
+        let m = null
         productInnerCart.forEach(element => {
-            m += element.price * element.count // генерация суммы товаров на основе количества и цен товаров 
+            m += element.price
         });
         cartBody.insertAdjacentHTML("beforeend", `
         В корзине: ${productInnerCart.length} товаров на сумму ${m} рублей
-        `) // вывод количества и суммы товаров .
+        `)
     }
 
 
@@ -103,19 +101,19 @@ function renderCart() { // функция генерация корзины
 }
 renderCart()
 
-function renderCatalog() { // функция генерации каталога на основе объекта cart
-    const catalogBody = document.querySelector("#catalog"); // получение блока каталога
+function renderCatalog() {
+    const catalogBody = document.querySelector("#catalog");
 
 
-    for (const product of basket.product) { // получение массива из объекта 
-        catalogBody.insertAdjacentHTML("afterbegin", `  
+    for (const product of basket.product) {
+        catalogBody.insertAdjacentHTML("afterbegin", `
         <div class="catalog__product">
             <h2 class="product-heading">${product.name}</h2>
             <img class="product-img" src="${product.img}" alt="${product.name}">
             <span class=product-price>${product.price}</span>
             <button>Добавить в корзину</button>
         </div>
-    `) // добавление разметки собраной на основе массива из объекта 
+    `)
 
 
     }
